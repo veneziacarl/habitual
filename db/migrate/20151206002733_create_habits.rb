4f3e2ea3142b06f6f00613a88adcd737afcc062b
Carl Venezia <veneziacarl@gmail.com>
2015-12-06 14:58:56 -0500

    cleared out all react componenets

--- db/migrate/20151206002733_create_habits.rb
+++ db/migrate/20151206002733_create_habits.rb
@@ -1,4 +1,8 @@
 class CreateHabits < ActiveRecord::Migration
   def change
+    create_table :habits do |t|
+      t.string :title, null: false
+      t.string :description, null: false
+    end
   end
 end
